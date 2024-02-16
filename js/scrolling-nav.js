17b148f9808c56b83b524338c8b9ce02fd19e74c
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2024-02-16 08:31:36 +0200

    Add files via upload

---
+++ js/scrolling-nav.js
@@ -0,0 +1,1 @@
+$(function(){$(document).on('click','a.page-scroll',function(event){var $anchor=$(this);$('html, body').stop().animate({scrollTop:$($anchor.attr('href')).offset().top},1500,'easeInOutExpo');event.preventDefault();});});
